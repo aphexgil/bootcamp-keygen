fee6856c8ed5c01442222b9932a9aee5f19ea48a
aphexgil <gil.young.1994@gmail.com>
2022-11-30 13:58:26 -0800

    Added index.html, style.css and script.js defining the password generating web page.

---
+++ script.js
@@ -0,0 +1,80 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+var possibleChars = "";
+var passwordLength;
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var numeric = "0123456789";
+var special = "`~!@#$%^&*()-_=+[{]}\|;:'?/>.<,";
+
+function getCharSet(){
+
+  var gotOne = false;
+
+  if(window.confirm("Include Upper Case Characters?")){
+    possibleChars += upperCase;
+    gotOne = true;
+  }
+
+  if(window.confirm("Include Lower Case Characters?")){
+    possibleChars += lowerCase;
+    gotOne = true;
+  }
+
+  if(window.confirm("Include Numeric Characters?")){
+    possibleChars += numeric;
+    gotOne = true;
+  }
+
+  if(window.confirm("Include Special Characters?")){
+    possibleChars += special;
+    gotOne = true;
+  }
+
+  if(!gotOne){
+    window.alert("Please include at least one type of character.");
+    return getCharSet();
+  }
+
+}
+
+function getLength(){
+  passwordLength = window.prompt("How Many Characters? (8-128)");
+  if(isNaN(passwordLength)){
+    window.alert("Please enter a number.");
+    return getLength();
+  }
+  passwordLength = Number(passwordLength);
+  if(passwordLength > 128 || passwordLength < 8){
+    window.alert("Number must be between 8 and 128.");
+    return getLength();
+  }
+}
+
+function generatePassword(){
+  var password = "";
+  var index = 0;
+  for(var i=0; i<passwordLength; i++){
+    index = Math.floor(Math.random() * possibleChars.length);
+    password += possibleChars[index];
+  }
+  return password;
+}
+
+getCharSet();
+getLength();
+writePassword();
